7d5e840517e7f7a6a69222a312c5dbf6b34b13b0
Guidzs <guidz.2004@gmail.com>
2023-02-15 18:13:51 -0300

    correção do package

---
+++ app/backend/src/tests/teams.test.ts
@@ -0,0 +1,8 @@
+import * as sinon from 'sinon';
+import * as chai from 'chai';
+// @ts-ignore
+import chaiHttp = require('chai-http');
+
+import { app } from '../app';
+chai.use(chaiHttp);
+const { expect } = chai;
